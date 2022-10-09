e857322cee04373ab2eccf38a76914b5e00314aa
GeronimoGallino <110981899+GeronimoGallino@users.noreply.github.com>
2022-10-09 16:41:19 -0300

    subo

---
+++ AppBTS/AppBTS/Negocio/HistorialBug.cs
@@ -0,0 +1,37 @@
+﻿using AppBTS.Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBTS.Negocio
+{
+    class HistorialBug
+    {
+        public int IdBug { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Responsable { get; set; }
+        public int Estado { get; set; }
+        public int AsignadoA { get; set; }
+        public int Prioridad { get; set; }
+        public int Criticidad { get; set; }
+        public int Producto { get; set; }
+
+        public DataTable RecuperarHistorialPorId(int idBug)
+        {
+            string consulta = "SELECT h.fecha_historico,u.usuario as responsable,e.nombre as estado,us.usuario as asignadoA"
+                            + " FROM (((BugsHistorico h"
+                            + " LEFT JOIN Usuarios u ON h.id_usuario_responsable=u.id_usuario)"
+                            + " JOIN Estados e ON h.id_estado=e.id_estado)"
+                            + " LEFT JOIN Usuarios us ON h.id_usuario_asignado=us.id_usuario)"
+                            + " WHERE h.id_bug=" + idBug;
+
+            DBHelper oDatos = new DBHelper();
+            return oDatos.consultarDB(consulta);
+        }
+    }
+}
---
+++ AppBTS/AppBTS/Presentacion/frmDetalleBug.cs
@@ -0,0 +1,74 @@
+﻿using AppBTS.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace AppBTS.Presentacion
+{
+    public partial class frmDetalleBug : Form
+    {
+        private int idBug;
+
+        //public int IdBug
+        //{
+        //    get { return idBug; }
+        //    set { idBug = value; }
+        //}
+
+        public frmDetalleBug(int idBug)
+        {
+            InitializeComponent();
+            this.idBug = idBug;
+        }
+        public frmDetalleBug()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDetalleBug_Load(object sender, EventArgs e)
+        {
+            this.CargarCampos(idBug);
+        }
+
+        private void CargarCampos(int idBug)
+        {
+            Bug oBugSeleccionado=new Bug();
+            DataTable dtBug = (oBugSeleccionado.RecuperarPorId(idBug));
+
+            txt_nro_bug.Text = dtBug.Rows[0]["id_bug"].ToString();
+            txt_titulo.Text = dtBug.Rows[0]["titulo"].ToString();
+            txt_producto.Text = dtBug.Rows[0]["producto"].ToString();
+            txt_fec_alta.Text = dtBug.Rows[0]["fecha_alta"].ToString();
+            txt_descri.Text=dtBug.Rows[0]["descripcion"].ToString();
+            txt_estado.Text = dtBug.Rows[0]["estado"].ToString();
+            txt_prioridad.Text = dtBug.Rows[0]["prioridad"].ToString();
+            txt_criticidad.Text = dtBug.Rows[0]["criticidad"].ToString();
+
+            HistorialBug oHistorialBug= new HistorialBug();
+            DataTable dtHistoBug=oHistorialBug.RecuperarHistorialPorId(idBug);
+            if(dtHistoBug.Rows.Count > 0)
+            {
+                for (int i = 0; i < dtHistoBug.Rows.Count; i++)
+                {
+                    dgv_historiales.Rows.Add(dtHistoBug.Rows[i]["fecha_historico"],
+                                             dtHistoBug.Rows[i]["responsable"],
+                                             dtHistoBug.Rows[i]["estado"],
+                                             dtHistoBug.Rows[i]["asignadoA"]);
+                }
+            }
+        }
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
